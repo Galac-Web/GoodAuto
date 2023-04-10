e6cd9ed44cf6e4d17e8d35e46c64684afd1f980a
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2023-04-10 14:26:21 +0300

    +firstBuild

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+function toggleClass() {
+  nav.classList.toggle("none"); // Переключаем класс hide
+  phone.classList.toggle("none");
+}
+
+//SLIDER
+
+let offset = 0; // from left
+let page = 0;
+let x;
+let xStart;
+let width;
+const sliderLine = document.querySelector(".sliderLine");
+// const sliderList = document.querySelectorAll(".dot");
+
+window.addEventListener("resize", init);
+init();
+
+const el = document.getElementById("sliderLine");
+el.addEventListener("touchmove", handleMove);
+el.addEventListener("touchstart", handleStart);
+el.addEventListener("touchend", handleEnd);
+
+document.querySelector(".next").addEventListener("click", Next);
+document.querySelector(".previous").addEventListener("click", Prev);
+
+function handleMove(event) {
+  x = event.touches[0].clientX;
+}
+function handleStart(event) {
+  xStart = event.touches[0].clientX;
+}
+function handleEnd() {
+  if (x < xStart) {
+    Next();
+  } else {
+    Prev();
+  }
+}
+function Next() {
+  offset -= width;
+  page++;
+  if (offset <= -width * 2) {
+    offset = 0;
+    page = 0;
+    // sliderList[4].classList.toggle("activeDot");
+  } else {
+    // sliderList[page - 1].classList.toggle("activeDot");
+  }
+  // sliderList[page].classList.toggle("activeDot");
+  sliderLine.style.left = offset + "px";
+}
+function Prev() {
+  offset += width;
+  page--;
+  if (offset > 0) {
+    offset = -width; //1484
+    page = 1;
+    // sliderList[0].classList.toggle("activeDot");
+  } else {
+    //  sliderList[page + 1].classList.toggle("activeDot");
+  }
+  sliderLine.style.left = offset + "px";
+  // sliderList[page].classList.toggle("activeDot");
+}
+function init() {
+  width = document.querySelector(".slider").offsetWidth;
+  console.log(width);
+}
